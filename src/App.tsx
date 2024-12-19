37b79608cfc9d0d87626304c4a08f2c51ddfd360
manovagyanik1 <52470178+manovagyanik1@users.noreply.github.com>
2024-12-19 22:09:29 +0530

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Terms } from './pages/Terms';
 import { Privacy } from './pages/Privacy';
+import { ThemeProvider } from './contexts/ThemeContext';
+
+function ScrollToTop() {
+  const { pathname, state } = useLocation();
+
+  useEffect(() => {
+    if (state?.scrollTo) {
+      const element = document.getElementById(state.scrollTo);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [pathname, state]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-        </Routes>
-      </Layout>
+      <ThemeProvider>
+        <ScrollToTop />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
