b3be5585130643f83c165dbbeca3527d80bc5792
manovagyanik1 <52470178+manovagyanik1@users.noreply.github.com>
2024-12-19 22:29:56 +0530

    Update src/components/Layout.tsx [BoltSync]

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
       <Header />
       <main className="flex-grow container mx-auto px-4 py-8">
         {children}
