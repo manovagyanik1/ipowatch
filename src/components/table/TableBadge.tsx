649b46c96311cda3e147b471ca5d30b5c34c8805
manovagyanik1 <52470178+manovagyanik1@users.noreply.github.com>
2024-12-19 21:24:37 +0530

    Update src/components/table/TableBadge.tsx [BoltSync]

--- src/components/table/TableBadge.tsx
+++ src/components/table/TableBadge.tsx
@@ -10,9 +10,10 @@ export const TableBadge: React.FC<BadgeProps> = ({ variant, value }) => {
     switch (variant) {
       case 'status':
         switch (value.toLowerCase()) {
-          case 'open': return 'bg-green-100 text-green-800';
+          case 'open':
+          case 'live': return 'bg-green-100 text-green-800';
           case 'upcoming': return 'bg-yellow-100 text-yellow-800';
-          case 'closed': return 'bg-gray-100 text-gray-800';
+          case 'closed': return 'bg-red-100 text-red-800';
           default: return 'bg-gray-100 text-gray-800';
         }
       case 'recommendation':
